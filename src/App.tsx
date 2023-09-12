441d33fddbef235f33fafab11bd3ddba4988b21f
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-12 18:57:41 -0300

    feat: create route Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from './components/Button'
+import { Button } from './components/Shared/Button'
 
 function App() {
   return <Button></Button>
--- src/components/Shared/Button/style.ts
+++ src/components/Shared/Button/style.ts
@@ -1,8 +1,5 @@
 import styled from 'styled-components'
 export const ButtonWithTheme = styled.button`
-  color: ${(props) => props.theme.color.secundary.main};
-  background-color: ${(props) => props.theme.color.secundary.contrastText};
-
   border: ${(props) => props.theme.border.style};
   border-radius: ${(props) => props.theme.border.radius};
 
