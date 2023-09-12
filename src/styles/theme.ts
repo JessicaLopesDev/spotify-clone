6cfc4d4390e7e352f16c9c800121b76305743b3d
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-12 18:48:03 -0300

    feat: setting theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,27 +1,9 @@
 const Theme = {
   color: {
-    background: '#141827',
-    primary: {
-      main: '#fffae3',
-      contrastText: '#c1d9d0',
-    },
-    secundary: {
-      main: '#736681',
-      contrastText: '#62455b',
-    },
+    background: '#000',
+    primary: '#121212',
+    secondary: '#727272',
   },
+}
 
-  border: {
-    radius: '15px',
-    style: '2px solid #736681',
-  },
-
-  font: {
-    small: '0.5rem',
-    default: '1rem',
-    big: '2rem',
-    bigger: '3rem',
-  },
-};
-
-export default Theme;
+export default Theme
